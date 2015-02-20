39eaf0de803d3b7fde4c628f7f5ffc27dcd21f22
Jacob Shorty <jakeshorty@gmail.com>
2015-02-20 14:23:22 -0500

    complete Hanoi

--- hanoi.js
+++ hanoi.js
@@ -1,8 +1,12 @@
+var readline = require('readline');
+var reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
 var Hanoi = function(){
   this.stacks = [[3,2,1],[],[]];
-  this.tower1 = this.stacks[0];
-  this.tower2 = this.stacks[1];
-  this.tower3 = this.stacks[2];
 };
 
 Hanoi.prototype.isWon = function() {
@@ -24,7 +28,7 @@ Hanoi.prototype.isValidMove = function(startTowerIdx, endTowerIdx) {
   if (end.length === 0) {
     return true;
   }
-  else if (end[end.length - 1] < start[start.length - 1]) {
+  else if (end[end.length - 1] > start[start.length - 1]) {
     return true;
   }
   else {
@@ -37,6 +41,47 @@ Hanoi.prototype.move = function(startTowerIdx, endTowerIdx) {
   var end = this.stacks[endTowerIdx];
 
   if (this.isValidMove(startTowerIdx, endTowerIdx)) {
-    end.push(start.pop())
+    end.push(start.pop());
+    return true;
   }
-}
+  return false;
+};
+
+Hanoi.prototype.print = function () {
+  console.log(JSON.stringify(this.stacks));
+};
+
+Hanoi.prototype.promptMove = function (callback) {
+  this.print();
+  reader.question("Where would you like to move from?", function (startTowerIdx) {
+    reader.question("Where would you like to move to?", function (endTowerIdx) {
+     var start = parseInt(startTowerIdx);
+     var end = parseInt(endTowerIdx);
+
+      callback(start, end);
+    });
+  });
+};
+
+Hanoi.prototype.run = function (completionCallback) {
+  var hanoi = this;
+  hanoi.promptMove(function (start, end) {
+    moved = hanoi.move(start, end)
+    if (!moved) {
+      console.log("Invalid move!");
+    }
+    if ( !hanoi.isWon() ) {
+      hanoi.run(completionCallback);
+    }
+    else {
+      completionCallback();
+    }
+  });
+};
+
+
+hanoi = new Hanoi();
+hanoi.run(function() {
+  console.log("Congrats! You've won!");
+  reader.close();
+});
